537eeb2f7626638e895d845f0ba397ed6586057e
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-21 23:47:20 +0200

    start lesson 14

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,43 @@
+
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
+console.log(40 + 8 + 23 - 10);
+
+console.log('Dima');
+console.log(23);
+
+let firstName = 'Di';
+let first = 'dimon';
+let firstNamePerson
+
+let person = 'dim4ik';
+let PI = 3.1415;
+let myFirstJob = 'Cook';
+let myCurrentJob = 'System Administrator';
+let futureJob = 'JavaScript Developer';
+
+
+console.log(firstName);
+console.log(myFirstJob);
+
+// console.log(true);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+console.log(typeof person);
+
+
+
+// let, const, var
+
+// can change - use let
+
+// cannot cachge - use const
+
+// old version of variables - var
+
+
+start lessons 14!!!
--- notes.js
+++ notes.js
@@ -3,4 +3,33 @@
 
 //Start 20 december 2022
 
-// Done with preparetion
+// Prinitive Data Types:
+1. Number
+let age = 34;
+
+2. String
+let firstName = 'Dima';
+
+3. Boolean
+let fullAge = true;
+
+4. Undefined // empty value
+let children;
+
+5. Null //empty value
+
+6. Symbol(ES2015)
+
+7. BigInt(ES2020)
+
+/**
+ *
+ * This
+ * is
+ * many
+ * lines
+ * comment
+ *
+ */
+
+// One line comment
