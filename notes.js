71fdedaf89e0ee0ff0248280227ef5bcfa9cbc0f
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-23 22:43:47 +0200

    lesson 15

--- notes.js
+++ notes.js
@@ -44,6 +44,45 @@ let children;
 // old version of variables - var
 
 
-start lessons 14!!!
-
 // Basic Operators
+
+
+// Math operators
+
+const now = 2037;
+const ageDima = now - 1988;
+const ageKsu = now - 1993;
+console.log(ageDima, ageKsu);
+console.log(ageDima * 2, ageDima / 10, 2 ** 3);
+
+// 2 ** 3 means 2 to the power of  3 = 2 * 2 * 2
+
+const firstName = 'Dima';
+const lastName = 'Selikhov';
+console.log(firstName + ' ' + lastName);
+
+
+
+// Assignment operators  
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--;
+x--;
+console.log(x);
+
+
+// Comparison operators
+console.log(ageDima > ageKsu); // <, >, >=, <=
+console.log(ageKsu >= 18);
+
+const isFullAge = ageKsu >= 18;
+
+console.log(now - 1993 > now - 2018)
+
+
+
+
+
