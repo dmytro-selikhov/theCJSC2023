460bd9afff98c0f30429f296db4a64a5fb2fb87a
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-23 16:09:04 +0200

    page 8

--- javaScriptFundamentalsPart1.js
+++ javaScriptFundamentalsPart1.js
@@ -143,8 +143,45 @@ if (numNeighbours === 1) {
   console.log('No borders')
 }
 
+/**
+ * LECTURE: Logical Operators
+1. Commentoutthepreviouscodesothepromptdoesn'tgetintheway
+2. Let'ssaySarahislookingforanewcountrytolivein.Shewantstoliveina
+country that speaks english, has less than 50 million people and is not an
+island.
+3. WriteanifstatementtohelpSarahfigureoutifyourcountryisrightforher.
+You will need to write a condition that accounts for all of Sarah's criteria. Take
+your time with this, and check part of the solution if necessary.
+4. Ifyoursistherightcountry,logastringlikethis:'YoushouldliveinPortugal:)'.If
+not, log 'Portugal does not meet your criteria :('
+5. Probablyyourcountrydoesnotmeetallthecriteria.Sogobackandtemporarily
+change some variables in order to make the condition true (unless you live in Canada :D)
+ */
 
-// page 7
+// Solution
+
+let country = 'Ukraine';
+const continent = 'Eurasia';
+let population = 43.81;
+
+console.log(country, continent, population);
+
+const isIsland = false;
+let language;
+
+console.log(isIsland, population, country, language);
+
+// Solution
+
+language = 'ukrainian';
+
+
+if (language === 'english' && population < 50 && isIsland == false) {
+  console.log(`You should live in ${country}:)`);
+} else {
+  console.log(`${country} does not meet your criteria :(`)
+}
+
 
 
 
