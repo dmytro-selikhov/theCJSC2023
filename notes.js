d262eb0b1928f8c95298d8cf16094912c1a3e8e4
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-12-28 12:06:40 +0200

    lesson 40

--- notes.js
+++ notes.js
@@ -456,7 +456,55 @@ console.log(yearsUntillRetirement(1970, 'Jake'));
 // lesson 38 Coding Challenge #1
 
 // lesson 39 Introduction to Arrays
-// test text
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// const years = new Array(1988, 1991, 2008, 2020);
+// console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Dima';
+const dima = [firstName, 'Selikhov', 2037 - 1988, 'system administrator', friends];
+console.log(dima);
+console.log(dima.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+// lesson 40 Basic Array Operations(Methods)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
