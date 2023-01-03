c00d2f364cfaabff57c28275a5c9671ea556e674
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-01-03 13:52:28 +0200

    lesson 63

--- notes.js
+++ notes.js
@@ -936,19 +936,56 @@ console.log(amplitudeBug);
 // § Data2:[12,5,-5,0,4]
 
 // Solution
+// 
+
+const printForecast = (arr) => {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `... ${arr[i]}oC in ${arr.indexOf(arr[i]) + 1} days `;
+  }
+  return str;
+}
+
+// printForecast([17, 21, 23]);
+// printForecast([12, 5, -5, 0, 4]x);
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
 
 
 
+// Mentors Solution
+
+// 1. Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2. Breaking up into sub-problems 
+// - Transform array into string 
+// - Transform each element to string with ºC
+// - Strinds needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+// console.log(`... ${data1[0]}ºC ...${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = '...';
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += ` ${arr[i]}ºC in ${i + 1} days ...`;
+  }
+  console.log('...' + str);
+  // console.log(str);
+}
+
+printForecast(data1);
 
 
 
-
-
-
-
-
-
-
+// lesson 63 Section Intro
 
 
 
