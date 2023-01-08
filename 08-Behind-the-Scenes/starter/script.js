a5556f146e6563cf63f323a76744f01aeaf8cd04
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-01-08 13:41:04 +0200

    lesson 96

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// 'sloppy mod'
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -37,3 +37,50 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+
+
+
+// Hoisting and TDZ in Practice
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Dima';
+let job = 'System Administrator';
+const year = 1991;
+
+
+// Functions 
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log(`All products deleted!`);
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
--- notes.js
+++ notes.js
@@ -1049,18 +1049,58 @@ first();
 
 // lesson 93 Scoping in Practice
 // lesson 94 Variable Environment: Hoisting and The TDZ
+// TDZ - Temporal Dead Zone
+
+// lesson 95 Hoisting and TDZ in Practice
 
 
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Dima';
+let job = 'System Administrator';
+const year = 1991;
 
 
+// Functions 
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
 
 
+function deleteShoppingCart() {
+  console.log(`All products deleted!`);
+}
 
 
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
 
 
-
-
+// lesson 96 The this Keyword
 
 
 
