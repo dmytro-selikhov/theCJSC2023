7bd622fe4ef2dbd39abd1a3b9c49a0c0346b9d79
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-01-21 21:18:53 +0200

    lesson 143

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,45 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+
+// SLICE does not change original array
+console.log(arr.slice(2)); // ['c', 'd', 'e']
+console.log(arr.slice(2, 4)); // ['c', 'd']
+console.log(arr.slice(-2)); // ['d', 'e']
+console.log(arr.slice(-1)); // ['e']
+console.log(arr.slice(1, -2)); // ['b', 'c']
+console.log(arr.slice()); // shallow copy of array - ['a', 'b', 'c', 'd', 'e']
+console.log([...arr]);// shallow copy of array - ['a', 'b', 'c', 'd', 'e']
+
+// SPLICE mutates original array
+// console.log(arr.splice(2)); // ['c', 'd', 'e']
+arr.splice(-1); // delete last element
+console.log(arr);
+arr.splice(1, 2); // delete 2 elements
+console.log(arr);
+
+// REVERSE mutates original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2); // ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+console.log(letters);
+console.log([...arr, ...arr2]); // ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+// JOIN
+console.log(letters.join(' - ')); // a - b - c - d - e - f - g - h - i - j
+
+
+
+
+
+
+
+
+
