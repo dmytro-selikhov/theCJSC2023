2370dc187daeed57f1f9635a6a89a414219ed10d
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-02-12 01:01:57 +0200

    task 192

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -164,16 +164,16 @@ const h1 = document.querySelector('h1');
 // });
 
 
-const alertH1 =  function(e){
-  alert('addEventListener: Great! You are reading the heading! :D');
+// const alertH1 =  function(e){
+//   alert('addEventListener: Great! You are reading the heading! :D');
   
-};
+// };
 
-h1.addEventListener('mouseenter', alertH1);
+// h1.addEventListener('mouseenter', alertH1);
 
 
 
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 4000);
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 4000);
 
 
 
@@ -182,6 +182,47 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 4000);
 //   alert('onmouseenter: Great! You are reading the heading! :D');
   
 // };
+
+
+// rgb(255, 255, 255);
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`; 
+
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function(e){
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
