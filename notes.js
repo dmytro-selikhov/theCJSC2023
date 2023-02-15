76e533c2304f8e3c2be5fe68d26635611e016fcd
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-02-15 14:36:48 +0200

    test text in progress

--- notes.js
+++ notes.js
@@ -1204,7 +1204,7 @@ console.log(z === window.z);
 // lesson 192 Event Delegation_ Implementing Page Navigation
 // lesson 193 DOM Traversing
 // lesson 194 Building a Tabbed Component
-// test text
+// test text in progress
 
 
 
