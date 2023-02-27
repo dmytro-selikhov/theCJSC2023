66f69aefd22deb31745b70b1b93e9642bda452a3
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-02-27 23:23:53 +0200

    task 200

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -207,7 +207,6 @@ headerObserver.observe(header);
 const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer){
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -223,6 +222,42 @@ allSections.forEach(function (section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+
+// Lazy loading images 
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImage = function(entries, observer) {
+  const [entry] = entries;
+
+  if(!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+
+};
+
+const imgObserver = new IntersectionObserver(loadImage, 
+{
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+ 
+
+
+
+
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 
