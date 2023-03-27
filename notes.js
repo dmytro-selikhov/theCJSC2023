db66813cd0e5cdb44ca80d510bb3f062494af0d4
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-03-27 22:35:08 +0300

    lesson 254

--- notes.js
+++ notes.js
@@ -1263,6 +1263,7 @@ console.log(z === window.z);
 // lesson 251 Promises and the Fetch API
 // lesson 252 Consuming Promises
 // lesson 253 Chaining Promises
+// lesson 254 Handling Rejected Promises
 
 
 
